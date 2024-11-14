43f6b047a75360f41c93ce70750fb16b87313a4c
Atharva <atharvabhise1@gmail.com>
2024-11-14 11:46:42 +0530

    v2

--- myfile2.py
+++ myfile2.py
@@ -6,3 +6,6 @@ print("Tuple with integers: ", int_tuple)
 
 mixed_tuple = (4, "Python", 9.3, 4, 9.8)
 print("Tuple with different data types: ", mixed_tuple)
+
+list1 = [1, 2, "Python", "Program", 15.9]
+list2 = ["Amy", "Ryan", "Henry", "Emma"]
