3c70134f44de91b65407089c81ba4cb27e68b522
Atharva <atharvabhise1@gmail.com>
2024-11-14 11:44:19 +0530

    v1

---
+++ myfile2.py
@@ -0,0 +1,8 @@
+empty_tuple = ()
+print("Empty tuple: ", empty_tuple)
+
+int_tuple = (4, 6, 8, 10, 12, 14)
+print("Tuple with integers: ", int_tuple)
+
+mixed_tuple = (4, "Python", 9.3, 4, 9.8)
+print("Tuple with different data types: ", mixed_tuple)
